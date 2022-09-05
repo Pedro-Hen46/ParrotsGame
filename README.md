39b9086e017bae355d27ee0f6d314b60b34880f8
Pedro Henrique <pedrodev@Pedros-MacBook-Pro.local>
2022-09-04 22:37:29 -0300

    Feat: ReadMe Ajust

---
+++ README.md
@@ -0,0 +1,36 @@
+
+<div style=" display: flex; flex-direction: column; align-items: center;"><img style="height:300px;" src="./lib/images/Logo.png">Jogo da memoria desenvolvido com tecnologias vanilla, clique no link do deploy e se divirta jogando Parrots Card Game. <br /></div><br />
+
+
+## Tecnologias 
+Algumas tecnologias usadas:
+
+* HTML5
+* JavaScript
+* CSS
+
+## Serviços
+* Github
+
+## Visão Geral do Game:
+
+<img style="width: 100%; border-radius: 10px;" src="./lib/images/apresentation.gif"><br />
+
+## Recursos
+
+Alguns recursos que são encontrados no projeto:
+ - Fazer pedidos de forma rápida e simples.
+ - Acabar com a fome de uma vez por todas.
+
+
+## Links
+  - Deseja jogar um pouco de Parrots Card Game? : https://pedro-hen46.github.io/ParrotsGame/
+  - Repositório: https://github.com/Pedro-Hen46/ParrotsGame
+
+  ## Autores
+
+  * **Pedro Henrique dos Santos Silva** 
+
+  <br />
+  Me siga no <a href="https://github.com/login?return_to=https%3A%2F%2Fgithub.com%2FPedro-Hen46" target="_blank"><strong>Github</strong> </a> e Faça uma conexão no <a href="https://github.com/pedro-Hen46/" target="_blank"><strong>LinkedIn</strong> </a>.<br/> 
+  Obrigado por visitar. ❤
---
+++ src/scripts.js
@@ -0,0 +1,168 @@
+//Algumas variaveis globais.
+let contadorJogadas = 0;
+let verificaSeGanhou = 0;
+
+//- Iniciando o game perguntando quantidade de cartas e verificando se é par.
+let qtdCartas = prompt("Quer jogar com quantas cartas? (de 4 a 14)");
+
+if (qtdCartas % 2 == 0 && qtdCartas >= 4 && qtdCartas <= 14) {
+  console.log("Iniciando o game, meu bom...");
+} else {
+  alert(
+    `Voce não escolheu um numero par dentro do intervalo, por favor tente novamente. Agora com um numero PAR entre o intervalo informado | 4 a 14 |.`
+  );
+  location.reload();
+}
+
+document.querySelector(
+  ".qtd-cartas"
+).innerText = `VOCÊ ESTA JOGANDO COM ${qtdCartas} CARTAS.`;
+
+const cardBoard = document.querySelector(".cardboard");
+
+let imagesCards = [
+  "./lib/images/bobrossparrot.gif",
+  "./lib/images/explodyparrot.gif",
+  "./lib/images/fiestaparrot.gif",
+  "./lib/images/metalparrot.gif",
+  "./lib/images/revertitparrot.gif",
+  "./lib/images/tripletsparrot.gif",
+  "./lib/images/unicornparrot.gif",
+  "./lib/images/bobrossparrot.gif",
+  "./lib/images/explodyparrot.gif",
+  "./lib/images/fiestaparrot.gif",
+  "./lib/images/metalparrot.gif",
+  "./lib/images/revertitparrot.gif",
+  "./lib/images/tripletsparrot.gif",
+  "./lib/images/unicornparrot.gif",
+];
+
+imprimirCartas();
+
+function controleGame() {
+  if (verificaSeGanhou == qtdCartas / 2) {
+    let button = document.querySelector(".button");
+    button.classList.remove("hidden");
+
+    const decisao = prompt(`
+        ✅🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩✅
+        🎉🎊    MEUS PARABENS VOCE GANHOU!    🎉🎊 
+        ✅🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩✅
+
+        TOTAL DE MOVIMENTOS (CARTAS) : ${contadorJogadas} ✅ VEZES 
+                    
+        DESEJA JOGAR NOVAMENTE? [SIM - NÃO]`);
+
+    if (decisao == "sim" || decisao == "SIM") {
+      alert(`🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩✅
+
+        INICIANDO NOVO JOGO | BOA SORTE 🌚`);
+      location.reload();
+    }
+  }
+}
+
+function comparador() {
+  const ShortCards = imagesCards;
+  ShortCards.sort(() => Math.random() - 0.5);
+}
+
+function imprimirCartas() {
+  const carta = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
+
+  for (let y = 0; y < qtdCartas; y++) {
+    let p = Math.trunc(Math.random() * qtdCartas);
+    let aux = carta[p];
+    carta[p] = carta[y];
+    carta[y] = aux;
+  }
+
+  for (let y = 0; y < qtdCartas; y++) {
+    if (carta[y] === carta[y + 1]) {
+      // Check lado a lado.
+      let aux = carta[y + 1];
+      carta[y] = carta[0];
+      carta[0] = aux;
+    }
+  }
+  for (let i = 0; i < qtdCartas; i++) {
+    document.querySelector(".baralho").innerHTML += `
+        <div class="memory-card" data-card="${imagesCards[carta[i]]}">
+            
+              <img class="front-face" src="./lib/images/front.png" width="97px" height="106px">      
+           
+            
+              <img class="back-face" src="${
+                imagesCards[carta[i]]
+              }" width="80px" height="100px">          
+            
+        </div>`;
+  }
+} //- final function Imprimir as Cartas...
+
+function checkaIgualdade() {
+  let deuMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+  if (!deuMatch) {
+    voltaAoOriginal();
+  } else {
+    resetCards(deuMatch);
+    verificaSeGanhou += 1;
+    setTimeout(controleGame, 1000);
+  }
+}
+
+const cards = document.querySelectorAll(".memory-card");
+let firstCard, secondCard, carton, cartonTwo;
+let lockCards = false;
+
+function viraAcarta() {
+  if (lockCards) return false;
+  this.classList.add("flip");
+
+  contadorJogadas += 1;
+
+  if (!firstCard) {
+    firstCard = this;
+    carton = firstCard;
+    carton = firstCard.querySelector(".back-face");
+    carton.classList.add("opaco-off");
+    return false;
+  }
+
+  secondCard = this;
+  cartonTwo = secondCard;
+
+  cartonTwo = cartonTwo.querySelector(".back-face");
+  cartonTwo.classList.add("opaco-off");
+  checkaIgualdade();
+}
+
+function voltaAoOriginal() {
+  lockCards = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    carton.classList.remove("opaco-off");
+    cartonTwo.classList.remove("opaco-off");
+    console.log(carton);
+    resetCards();
+  }, 1000);
+}
+
+function resetCards(deuMatch = false) {
+  if (deuMatch) {
+    firstCard.removeEventListener("click", viraAcarta);
+    secondCard.removeEventListener("click", viraAcarta);
+  }
+
+  [firstCard, secondCard, lockCards] = [null, null, false];
+}
+
+cards.forEach((card) => card.addEventListener("click", viraAcarta));
+
+function restartGame() {
+  if (window.confirm("Deseja reiniciar o game?")) {
+    location.reload();
+  }
+}
